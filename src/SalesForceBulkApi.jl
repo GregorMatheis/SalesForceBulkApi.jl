ba767b64f893aade5160536a9e10216a7e2c0bc2
Gregor Matheis <MatheG01@stepstone.com>
2019-05-17 13:54:46 +0200

    handling of quited missings in csv return

--- src/SalesForceBulkApi.jl
+++ src/SalesForceBulkApi.jl
@@ -130,7 +130,7 @@ function results(session, batch)
                 ["Content-Type" => "text/plain",
                 "X-SFDC-Session" => session["sessionId"]])
     status = ret.status;
-    body = CSV.read(IOBuffer(String(ret.body)), missingstring = "")
+    body = mapcols(x -> replace(x, "" => missing), CSV.read(IOBuffer(String(ret.body)), missingstring = ""))
     return body
 end
 
